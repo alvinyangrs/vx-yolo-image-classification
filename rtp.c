66c3a5ca6cac42d71e9b9a32d026d21886793a72
Rensheng Yang <rensheng.yang@windriver.com>
2024-11-03 15:07:52 +0800

    workable image classification rtp

---
+++ rtp.c
@@ -0,0 +1,13 @@
+/* includes */
+
+#include <stdio.h>
+
+int main (
+    int	   argc,	/* number of arguments */
+    char * argv[]	/* array of arguments */
+    ) {
+
+	printf("The Hello World is from VxWorks RTP !\n");
+
+    return 0;
+}
